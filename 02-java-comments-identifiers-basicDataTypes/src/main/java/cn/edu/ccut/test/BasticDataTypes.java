2d0594bc0769f0ef74d42be59fca0cafbf1756c9
wangjian136 <1451518614@qq.com>
2019-11-08 21:37:22 +0800

    comments-identifiers-basicDataTypes

---
+++ 02-java-comments-identifiers-basicDataTypes/src/main/java/cn/edu/ccut/test/BasticDataTypes.java
@@ -0,0 +1,23 @@
+package cn.edu.ccut.test;
+
+public class BasticDataTypes {
+
+	public static void main(String[] args) {
+		//Bastic
+			//Numerical type
+				//Integer type
+				byte a = 1;
+				short b = 2;
+				int c = 3;
+				long d = 4;
+				//Floating point type
+				float e = 5.60f;
+				double f = 7.34;
+			//Character type
+			char g = 'a';
+			//Boolean type
+			boolean h = true;
+			
+	}
+
+}
