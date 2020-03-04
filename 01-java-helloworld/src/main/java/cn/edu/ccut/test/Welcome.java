fd0fb18059ccd2b7edfb9300ac85033605d92405
wangjian136 <1451518614@qq.com>
2020-03-04 17:21:25 +0800

    change something

--- 01-java-helloworld/src/main/java/cn/edu/ccut/test/Welcome.java
+++ 01-java-helloworld/src/main/java/cn/edu/ccut/test/Welcome.java
@@ -1,9 +1,12 @@
 package cn.edu.ccut.test;
 
+import java.util.Arrays;
+
 public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("hello world !");
+		System.out.println(Arrays.toString(args));
 	}
 
 }
